dc4265e91e3a8be7a47982cc20bcdda31fe8af26
danjor667 <jordannguepi@gmail.com>
2025-04-03 23:13:28 +0100

    requirements v3

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-figy56urcc-hdvj36!^6u%1(a+ece(7#$x4cec&&&nxk%hrb53
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
